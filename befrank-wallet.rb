f4772dd3d8d85dd2301ec346a58786410d16b624
Damien Trog <dtrog@me.com>
2018-03-02 09:55:13 +0100

    Installs the befrank-wallet-qt.app

--- befrank-wallet.rb
+++ befrank-wallet.rb
@@ -11,16 +11,11 @@ class BefrankWallet < Formula
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
+    prefix.install "befrank-wallet-qt.app"
+    bin.write_exec_script "#{prefix}/befrank-wallet-qt.app/Contents/MacOS/befrank-wallet-qt"
   end
 
   test do
-    cmd = "#{bin}befrank-wallet --restore-deterministic-wallet "\
-    "--password brew-test --restore-height 1 --generate-new-wallet wallet "\
-    "--electrum-seed 'buffet jagged spiders cabin anybody fully gifts thaw"\
-    " maps ember ghetto pencil wetsuit vinegar match alerts upright wade "\
-    "benches vinegar bugs knapsack enraged fuel jagged'"\
-    "--command address"
-    address = "FX1vEdZf6dmA6SKgwxSeng12Uvza6i4GMUZmeS4No3j7HzYAkoRKQ74G3nogEKsfoy4P5dvT77BCZZRwYb9UXwWTHzygVji"
-    assert_equal address, shell_output(cmd).split[-1]
+    assert_predicate testpath/"#{bin}/befrank-wallet-qt.app", :exist?, "I'm an untestable GUI app."
   end
 end
