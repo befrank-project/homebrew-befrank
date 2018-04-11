0ebc8eb4c6b5063b03ba73a29c8358ee0bb45ecf
Damien <damien.trog@gmail.com>
2018-04-11 07:09:06 +0200

    On our way to a stabler blockchain

--- befrank.rb
+++ befrank.rb
@@ -1,8 +1,8 @@
 class Befrank < Formula
   desc "Command line tools (daemon, wallet and miner) for the Belgian eFrank. A crypto-currency made for and by Belgium and anyone who feels part of our community."
   homepage "http://getfrank.be"
-  url "https://github.com/befrank-project/befrank/archive/v0.1.1-alpha.3.tar.gz"
-  sha256 "ab64b6a2cfd9826749878eb99ba9382a16f971e11a649bc4003a9941cf4e86e3"
+  url "https://github.com/befrank-project/befrank/archive/v0.1.1-alpha.4.tar.gz"
+  sha256 "fed9f1e407f67f5cf28b53a455ebf2005d7a7bd2798b2aa731d695e8845343c7"
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "boost"
