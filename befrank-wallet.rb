6f210170720c9d41dcfb016f74d48742233edc5b
Damien <damien.trog@gmail.com>
2018-04-11 07:01:52 +0200

    Onto alpha 4

--- befrank-wallet.rb
+++ befrank-wallet.rb
@@ -1,8 +1,8 @@
 class BefrankWallet < Formula
   desc "GUI wallet for the Belgian eFrank. A crypto-currency made for and by Belgians and anyone who feels part of our community."
   homepage "http://getfrank.be"
-  url "https://github.com/befrank-project/befrank-wallet/releases/download/v0.1.1-alpha.3/befrank-wallet-v0.1.1-alpha.3.tar.gz"
-  sha256 "c2d8a0dcc2d8066417669daeed7c48363d4ecf81b23518a7c73eaa316366ab2b"
+  url "https://github.com/befrank-project/befrank-wallet/releases/download/v0.1.1-alpha.4/befrank-wallet-v0.1.1-alpha.4.tar.gz"
+  sha256 "778661c9a87ec5af824d3f20ec461a9564d29da53bf1b6987c54891c95373bc8"
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "boost"
